33e0dc8cc77752b9a16e774a48fa8758082254d9
Vaibhav Srivastav <vaibhavs10@gmail.com>
2023-09-14 14:18:14 +0200

    up

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from TTS.api import TTS
 
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v1", gpu=True)
 
-def predict(prompt, audio_file_pth):
+def predict(prompt, language, audio_file_pth):
 
     tts.tts_to_file(text=prompt,
                 file_path="output.wav",
                 speaker_wav=audio_file_pth,
-                language="en")
+                language=language)
 
     return gr.make_waveform(audio="output.wav",)
 
@@ -19,6 +19,7 @@ gr.Interface(
     fn=predict,
     inputs=[
         gr.Textbox(label="Prompt", info = "One or two sentences at a time is better* (max: 10)", placeholder = "It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",),
+        gr.Dropdown(choices=["en", "es", "fr", "de", "it", "pt", "pl", "tr", "ru", "nl", "cz", "ar", "zh"], max_choices=1),        
         gr.Audio(label="Upload Speaker WAV", type="filepath"),
     ],
     outputs=[
